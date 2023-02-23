02dfbcc3236e37e617a80deade537354fa8db9b5
Francois Touchard <francois.touchard.wos@gmail.com>
2023-02-23 11:11:57 +0100

    search with resized gifs

--- gifs_library/src/components/search/Search.js
+++ gifs_library/src/components/search/Search.js
@@ -10,18 +10,17 @@ const Search = () => {
   useEffect(() => {
     async function getGifs() {
       const response = await fetch(
-        //   "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=iitVv68p2nod2xg57ktamEzDXfoSmsfa&limit=5"
-        `http://api.giphy.com/v1/gifs/search?q=${userQuery}&api_key=iitVv68p2nod2xg57ktamEzDXfoSmsfa&limit=5`
+        `http://api.giphy.com/v1/gifs/search?q=${userQuery}&api_key=iitVv68p2nod2xg57ktamEzDXfoSmsfa&limit=10`
       );
       const res = await response.json();
-      console.log("RES", res);
+      //   console.log("RES", res);
       setGifs(res.data);
       // console.log("ONE", data.data[0].bitly_url)
     }
     getGifs();
   }, [userQuery]);
 
-  console.log("la data", gifs);
+    console.log("la data", gifs);
 
   return (
     <div className="searchContainer">
--- gifs_library/src/components/search/searchResults/SearchResults.js
+++ gifs_library/src/components/search/searchResults/SearchResults.js
@@ -1,10 +1,11 @@
 const SearchResults = ({ gifs }) => {
-  console.log("LALALA", gifs);
+  //   console.log("LALALA", gifs);
   return (
     <div className="searchResultsContainer">
-      <p> hellooooo </p>
+      <p> Search Results </p>
       {gifs.map((gif) => (
         <img key={gif.id} src={gif.images.fixed_height.url} alt={gif.title} />
+        // <img key={gif.id} src={gif.images.fixed_height_small.url} alt={gif.title} />
       ))}
     </div>
   );
